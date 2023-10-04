46ac9bd9740dc2b77a36224851bac5959536c9f7
Leonardo Carrijo <lh83712@gmail.com>
2023-10-04 19:24:47 -0300

    Update pesos.py

--- Exercícios/pesos.py
+++ Exercícios/pesos.py
@@ -1,15 +1,6 @@
-w = 100
-r = 4
+def triangulo(a, b, c):
+    sides = sorted([a, b, c])
+    if sides[2] >= (sides[1] + sides[0]): return False
+    return True
 
-def peso(w, r):
-    if r == 1:
-        return w
-    elif r == 0:
-        return 0
-    epley = w * ( 1 + r / 30)
-    McGlothin = 100 * w / 101.3 - 2.67123 * r
-    Lombardi = w * r ** 0.10
-    lista = [round(epley), round(McGlothin), round(Lombardi)]
-    return max(lista)
-
-print(peso(w, r))
+print(triangulo(3, 1, 2))
